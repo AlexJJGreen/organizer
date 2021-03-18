977528697c70f9c27cdabf50a14a5b3e20758035
alexjjgreen <alexjjgreen@gmail.com>
2021-03-18 22:20:20 +0100

    login_form logic and view added

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,18 @@
 from app import app
 from flask import render_template
 from app.calendars import get_month, TODAY
+from app.forms import LoginForm
 
 @app.route("/index")
 @app.route("/")
 def index():
+    subtitle = "Home"
     today = TODAY.day
     month = get_month(TODAY)
-    subtitle = "Home"
     return render_template("index.html", subtitle=subtitle, month=month, today=today)
+
+@app.route("/login")
+def login(methods=["GET", "POST"]):
+    subtitle = "Login"
+    form = LoginForm()
+    return render_template("login.html", subtitle=subtitle, form=form)
